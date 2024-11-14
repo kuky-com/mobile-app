44134a45d88cd54af05cd950fb66b50034fb8757
Enache Adelina <enache.adelina99@gmail.com>
2024-11-14 22:03:05 +0200

    Add react query

--- App.js
+++ App.js
@@ -1,45 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { Dimensions, StyleSheet, View } from 'react-native';
-import { useFonts } from 'expo-font';
-import Text from '@/components/Text';
-import MainApp from '@/screens'
-import { NavigationContainer } from '@react-navigation/native';
-import Toast, { BaseToast } from 'react-native-toast-message'
-import images from '@/utils/images';
-import { Image } from 'expo-image';
-import { isReadyRef, navigationRef } from '@/utils/NavigationService';
-import { Provider } from 'jotai';
-import { storeAtom } from '@/actions/global';
-import './sheets'
-import { SheetProvider } from "react-native-actions-sheet"
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { Suspense, useEffect } from 'react';
-import axios, { Axios } from 'axios';
-import dayjs from 'dayjs'
-import * as Linking from 'expo-linking';
-import Smartlook from 'react-native-smartlook-analytics'
-import { AEMReporterIOS, AppEventsLogger, Settings } from 'react-native-fbsdk-next';
-import { requestTrackingPermissionsAsync } from 'expo-tracking-transparency';
-import { AlertProvider } from '@/components/AlertProvider';
-import { AppUpdateAlertProvider } from '@/components/AppUpdateAlert';
-import { Amplify } from 'aws-amplify';
-import amplifyconfig from './src/amplifyconfiguration.json';
+import { StatusBar } from "expo-status-bar";
+import { Dimensions, StyleSheet, View } from "react-native";
+import { useFonts } from "expo-font";
+import Text from "@/components/Text";
+import MainApp from "@/screens";
+import { NavigationContainer } from "@react-navigation/native";
+import Toast, { BaseToast } from "react-native-toast-message";
+import images from "@/utils/images";
+import { Image } from "expo-image";
+import { isReadyRef, navigationRef } from "@/utils/NavigationService";
+import { Provider } from "jotai";
+import { storeAtom } from "@/actions/global";
+import "./sheets";
+import { SheetProvider } from "react-native-actions-sheet";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Suspense, useEffect } from "react";
+import axios, { Axios } from "axios";
+import dayjs from "dayjs";
+import * as Linking from "expo-linking";
+import Smartlook from "react-native-smartlook-analytics";
+import { AEMReporterIOS, AppEventsLogger, Settings } from "react-native-fbsdk-next";
+import { requestTrackingPermissionsAsync } from "expo-tracking-transparency";
+import { AlertProvider } from "@/components/AlertProvider";
+import { AppUpdateAlertProvider } from "@/components/AppUpdateAlert";
+import { Amplify } from "aws-amplify";
+import amplifyconfig from "./src/amplifyconfiguration.json";
+import { useQueryClient, QueryClientProvider } from "@tanstack/react-query";
 Amplify.configure(amplifyconfig);
 
-Smartlook.instance.preferences.setProjectKey(
-  '6bebfbc50c0aedc486a2766bc51c24d0d2b4a13f'
-);
+Smartlook.instance.preferences.setProjectKey("6bebfbc50c0aedc486a2766bc51c24d0d2b4a13f");
 Smartlook.instance.start();
 
-const prefix = Linking.createURL('/');
+const prefix = Linking.createURL("/");
 
-var relativeTime = require('dayjs/plugin/relativeTime')
-var customParseFormat = require("dayjs/plugin/customParseFormat")
+var relativeTime = require("dayjs/plugin/relativeTime");
+var customParseFormat = require("dayjs/plugin/customParseFormat");
 
-dayjs.extend(relativeTime)
-dayjs.extend(customParseFormat)
+dayjs.extend(relativeTime);
+dayjs.extend(customParseFormat);
 
-{/* <BaseToast
+const queryClient = useQueryClient();
+
+{
+  /* <BaseToast
       {...props}
       style={{ padding: 0, margin: 0, backgroundColor: '#725ED4', width: Dimensions.get('screen').width, height: Dimensions.get('screen').height, alignItems: 'center' }}
       contentContainerStyle={{ padding: 0, margin: 0, backgroundColor: '#725ED4', width: Dimensions.get('screen').width, height: Dimensions.get('screen').height, alignItems: 'center' }}
@@ -47,72 +49,146 @@ dayjs.extend(customParseFormat)
         fontSize: 15,
         fontWeight: '400'
       }}
-    /> */}
+    /> */
+}
 const toastConfig = {
   sent: ({ text1, props, text2 }) => (
-    <View style={{ flex: 1, padding: 16, margin: 0, gap: 32, backgroundColor: '#725ED4', width: Dimensions.get('window').width, height: Dimensions.get('window').height, alignItems: 'center', justifyContent: 'center' }}>
-      <StatusBar backgroundColor='#725ED4' />
-      <Image source={images.sent_icon} style={{ width: 120, height: 120 }} contentFit='contain' />
-      <Text style={{ fontSize: 20, color: 'white', fontWeight: 'bold', textAlign: 'center' }}>{text1}</Text>
-      <Text style={{ fontSize: 18, color: 'white', fontWeight: 'bold', textAlign: 'center' }}>{text2}</Text>
+    <View
+      style={{
+        flex: 1,
+        padding: 16,
+        margin: 0,
+        gap: 32,
+        backgroundColor: "#725ED4",
+        width: Dimensions.get("window").width,
+        height: Dimensions.get("window").height,
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <StatusBar backgroundColor="#725ED4" />
+      <Image source={images.sent_icon} style={{ width: 120, height: 120 }} contentFit="contain" />
+      <Text
+        style={{
+          fontSize: 20,
+          color: "white",
+          fontWeight: "bold",
+          textAlign: "center",
+        }}
+      >
+        {text1}
+      </Text>
+      <Text
+        style={{
+          fontSize: 18,
+          color: "white",
+          fontWeight: "bold",
+          textAlign: "center",
+        }}
+      >
+        {text2}
+      </Text>
     </View>
   ),
   deny: ({ text1, props, text2 }) => (
-    <View style={{ flex: 1, padding: 16, margin: 0, gap: 32, backgroundColor: '#725ED4', width: Dimensions.get('window').width, height: Dimensions.get('window').height, alignItems: 'center', justifyContent: 'center' }}>
-      <Image source={images.deny_icon} style={{ width: 120, height: 120 }} contentFit='contain' />
+    <View
+      style={{
+        flex: 1,
+        padding: 16,
+        margin: 0,
+        gap: 32,
+        backgroundColor: "#725ED4",
+        width: Dimensions.get("window").width,
+        height: Dimensions.get("window").height,
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Image source={images.deny_icon} style={{ width: 120, height: 120 }} contentFit="contain" />
     </View>
   ),
   review_sent: ({ text1, props, text2 }) => (
-    <View style={{ flex: 1, padding: 16, margin: 0, gap: 32, backgroundColor: '#000000aa', width: Dimensions.get('window').width, height: Dimensions.get('window').height, alignItems: 'center', justifyContent: 'center' }}>
-      <StatusBar backgroundColor='#725ED4' />
-      <View style={{ backgroundColor: '#725ED4', width: '100%', gap: 20, paddingHorizontal: 16, paddingVertical: 48, borderRadius: 16 }}>
-        <Text style={{ fontSize: 20, color: 'white', fontWeight: 'bold', textAlign: 'center' }}>Thank you for your feedback!</Text>
-        <Text style={{ fontSize: 16, lineHeight: 18, color: 'white', fontWeight: 'bold', textAlign: 'center' }}>{`Your review has been submitted.\nYour input helps us build a supportive and positive community.`}</Text>
+    <View
+      style={{
+        flex: 1,
+        padding: 16,
+        margin: 0,
+        gap: 32,
+        backgroundColor: "#000000aa",
+        width: Dimensions.get("window").width,
+        height: Dimensions.get("window").height,
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <StatusBar backgroundColor="#725ED4" />
+      <View
+        style={{
+          backgroundColor: "#725ED4",
+          width: "100%",
+          gap: 20,
+          paddingHorizontal: 16,
+          paddingVertical: 48,
+          borderRadius: 16,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 20,
+            color: "white",
+            fontWeight: "bold",
+            textAlign: "center",
+          }}
+        >
+          Thank you for your feedback!
+        </Text>
+        <Text
+          style={{
+            fontSize: 16,
+            lineHeight: 18,
+            color: "white",
+            fontWeight: "bold",
+            textAlign: "center",
+          }}
+        >{`Your review has been submitted.\nYour input helps us build a supportive and positive community.`}</Text>
       </View>
     </View>
   ),
-}
+};
 
 export default function App() {
-
   useEffect(() => {
-    Settings.initializeSDK()
-    Settings.setAutoLogAppEventsEnabled(true)
-    Settings.setAdvertiserTrackingEnabled(true)
-    Settings.setAdvertiserIDCollectionEnabled(true)
-
+    Settings.initializeSDK();
+    Settings.setAutoLogAppEventsEnabled(true);
+    Settings.setAdvertiserTrackingEnabled(true);
+    Settings.setAdvertiserIDCollectionEnabled(true);
 
     const loadAppEvent = async () => {
       const { status } = await requestTrackingPermissionsAsync();
-      Settings.setAppID('1051248213166532');
+      Settings.setAppID("1051248213166532");
 
-      if (status === 'granted') {
+      if (status === "granted") {
         await Settings.setAdvertiserTrackingEnabled(true);
       }
 
-      AppEventsLogger.logEvent('start_app_event');
-      AEMReporterIOS.logAEMEvent('start_app_event', 1)
-      console.log('log start event')
-    }
+      AppEventsLogger.logEvent("start_app_event");
+      AEMReporterIOS.logAEMEvent("start_app_event", 1);
+      console.log("log start event");
+    };
 
-    loadAppEvent()
-  }, [])
+    loadAppEvent();
+  }, []);
 
   const linking = {
-    prefixes: [
-      prefix,
-      'http://kuky.com',
-      'kuky://kuky.com'
-    ],
+    prefixes: [prefix, "http://kuky.com", "kuky://kuky.com"],
   };
 
-
   const [loaded, error] = useFonts({
-    'Comfortaa-Light': require('./src/assets/fonts/Comfortaa-Light.ttf'),
-    'Comfortaa-Regular': require('./src/assets/fonts/Comfortaa-Regular.ttf'),
-    'Comfortaa-Medium': require('./src/assets/fonts/Comfortaa-Medium.ttf'),
-    'Comfortaa-SemiBold': require('./src/assets/fonts/Comfortaa-SemiBold.ttf'),
-    'Comfortaa-Bold': require('./src/assets/fonts/Comfortaa-Bold.ttf'),
+    "Comfortaa-Light": require("./src/assets/fonts/Comfortaa-Light.ttf"),
+    "Comfortaa-Regular": require("./src/assets/fonts/Comfortaa-Regular.ttf"),
+    "Comfortaa-Medium": require("./src/assets/fonts/Comfortaa-Medium.ttf"),
+    "Comfortaa-SemiBold": require("./src/assets/fonts/Comfortaa-SemiBold.ttf"),
+    "Comfortaa-Bold": require("./src/assets/fonts/Comfortaa-Bold.ttf"),
   });
 
   return (
@@ -125,12 +201,15 @@ export default function App() {
               linking={linking}
               onReady={() => {
                 isReadyRef.current = true;
-              }}>
-              <StatusBar translucent style='dark' />
+              }}
+            >
+              <StatusBar translucent style="dark" />
               <Suspense>
-                <Provider store={storeAtom}>
-                  <MainApp />
-                </Provider>
+                <QueryClientProvider client={queryClient}>
+                  <Provider store={storeAtom}>
+                    <MainApp />
+                  </Provider>
+                </QueryClientProvider>
               </Suspense>
               <Toast config={toastConfig} />
             </NavigationContainer>
@@ -144,8 +223,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
