d0857b35bfa8e37fee5de41b27240797d2f645e4
dusankekic8888 <dusankekic8888@gmail.com>
2025-06-11 09:34:18 +0700

    fix: update android files, config gradlew, add eas.json

--- android/app/src/main/java/com/kuky/MainApplication.kt
+++ android/app/src/main/java/com/kuky/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.kuky.android
+package com.kuky
 
 import android.app.Application
 import android.content.res.Configuration
--- babel.config.js
+++ babel.config.js
@@ -8,11 +8,17 @@ const MODULE_RESOLVER = [
   },
 ];
 
-module.exports = {
-  presets: ["module:metro-react-native-babel-preset", "nativewind/babel"],
-  plugins: [
-    MODULE_RESOLVER,
-    "react-native-reanimated/plugin",
-    ["@babel/plugin-transform-private-methods", { loose: true }],
-  ],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: [
+      "module:metro-react-native-babel-preset",
+      "nativewind/babel", // nếu bạn đang dùng Tailwind
+    ],
+    plugins: [
+      MODULE_RESOLVER,
+      ["@babel/plugin-transform-private-methods", { loose: true }],
+      "react-native-reanimated/plugin", // Luôn để plugin này cuối cùng
+    ],
+  };
 };
